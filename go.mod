1e1bf3a75ed44f2388062eb9f61707e05fdca9a0
tiredsosha <tiredsosha@gmail.com>
2024-07-02 17:13:40 +0100

    да похуй, что то наделала с мктт

--- go.mod
+++ go.mod
@@ -1,3 +1,37 @@
 module github.com/tiredsosha/admin
 
-go 1.19
+go 1.22
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.1
+	github.com/go-ole/go-ole v1.2.6
+	github.com/moutend/go-wca v0.2.0
+)
+
+require (
+	github.com/getlantern/context v0.0.0-20220418194847-3d5e7a086201 // indirect
+	github.com/getlantern/errors v1.0.3 // indirect
+	github.com/getlantern/golog v0.0.0-20211223150227-d4d95a44d873 // indirect
+	github.com/getlantern/hex v0.0.0-20220104173244-ad7e4b9194dc // indirect
+	github.com/getlantern/hidden v0.0.0-20220104173330-f221c5a24770 // indirect
+	github.com/getlantern/ops v0.0.0-20220713155959-1315d978fff7 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	go.opentelemetry.io/otel v1.10.0 // indirect
+	go.opentelemetry.io/otel/trace v1.10.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
+)
+
+require (
+	github.com/getlantern/systray v1.2.1
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e
+	golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
+	golang.org/x/sync v0.0.0-20220907140024-f12130a52804 // indirect
+	golang.org/x/sys v0.0.0-20220915200043-7b5979e65e41 // indirect
+	gopkg.in/yaml.v3 v3.0.1
+)
---
+++ tools/configurator/file.go
@@ -0,0 +1,81 @@
+package configurator
+
+import (
+	"errors"
+	"os"
+	"reflect"
+	"strings"
+
+	"github.com/tiredsosha/warden/tools/logger"
+	"gopkg.in/yaml.v3"
+)
+
+// структура конфига
+type conf struct {
+	Broker   string `yaml:"broker"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Port     int    `yaml:"httpPort"`
+}
+
+// поиск конфига на диске
+func getConf(file string, cnf interface{}) error {
+	yamlFile, err := os.ReadFile(file)
+	if err == nil {
+		err = yaml.Unmarshal(yamlFile, cnf)
+	}
+
+	return err
+}
+
+// валидация конфига, валидация проходится если все заполнено
+func validateConf(cfg *conf) error {
+	var err error
+	v := reflect.ValueOf(*cfg)
+	typeOfS := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		field := typeOfS.Field(i).Name
+		value := v.Field(i).Interface()
+		if value == "" {
+			err = errors.New("config: " + strings.ToLower(field) + " field is emtpy/nonexist")
+			break
+		}
+		err = nil
+	}
+	return err
+}
+
+// создание дефолтного конфига и запись его в файл
+func confFile() *conf {
+	logger.Warn.Println("making a default config")
+	confDef := conf{
+		Broker:   "127.0.0.1",
+		Username: "admin",
+		Password: "password",
+		Port:     8080,
+	}
+
+	yamlData, _ := yaml.Marshal(confDef)
+	err := os.WriteFile("config.yaml", yamlData, 0644)
+	if err != nil {
+		logger.Error.Fatal("can't to write default conf into the file")
+	}
+	return &confDef
+}
+
+func ConfInit() *conf {
+	// создаем пустую версию конфига, или ссылку на него
+	cfg := &conf{}
+	// если у нас нет файла конфиг, то мы создаем дефотный конфиг
+	if err := getConf("config.yaml", cfg); err != nil {
+		logger.Error.Println(err)
+		cfg = confFile()
+	}
+	// если у нас есть конфиг и он не проходит валидацию, то вы выходим из приложенияЫ
+	if err := validateConf(cfg); err != nil {
+		logger.Error.Println(err)
+		logger.Error.Fatal("EXITING")
+	}
+	return cfg
+}
