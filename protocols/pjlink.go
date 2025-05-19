480755e951b649ee2616bfa5cbac97ed530c7f4e
tiredsosha <tiredsosha@gmail.com>
2025-05-19 22:27:15 +0300

    проектор дебаг

--- protocols/pjlink.go
+++ protocols/pjlink.go
@@ -23,21 +23,55 @@ func SendPjlink(ip, command string) {
 
 }
 
+// func GetPjlink(ip string) int {
+// 	response := 520
+// 	proj := pjlink.NewProjector(ip, "")
+// 	status, err := proj.GetPowerStatus()
+// 	if err != nil {
+// 		logger.Error.Printf("couldn't send execute pjlink %v", err)
+// 	} else {
+// 		if len(status.Response) > 0 {
+// 			//logger.Info.Println("pjlink status -", status.Response)
+// 			boolStatus, _ := strconv.ParseBool(status.Response[0])
+// 			if boolStatus {
+// 				response = 200
+// 			} else {
+// 				response = 521
+// 			}
+// 		}
+// 	}
+// 	logger.Info.Println("pjlink status -", response)
+// 	return response
+// }
+
 func GetPjlink(ip string) int {
+	defer func() {
+		if r := recover(); r != nil {
+			// Log the panic or handle it as needed
+			logger.Error.Printf("Recovered from panic in GetPjlink: %v", r)
+		}
+	}()
+
 	response := 520
 	proj := pjlink.NewProjector(ip, "")
 	status, err := proj.GetPowerStatus()
 	if err != nil {
 		logger.Error.Printf("couldn't send execute pjlink %v", err)
 	} else {
+		//logger.Info.Println("pjlink status -", status.Response)
 		if len(status.Response) > 0 {
-			//logger.Info.Println("pjlink status -", status.Response)
-			boolStatus, _ := strconv.ParseBool(status.Response[0])
-			if boolStatus {
-				response = 200
+			boolStatus, err := strconv.ParseBool(status.Response[0])
+			if err != nil {
+				logger.Error.Printf("Error parsing bool from response: %v", err)
 			} else {
-				response = 521
+				if boolStatus {
+					response = 200
+				} else {
+					response = 521
+				}
 			}
+		} else {
+			logger.Error.Println("Empty response received")
 		}
 	}
 	logger.Info.Println("pjlink status -", response)
