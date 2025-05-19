3e3b810cf8f87b250360ad58d9bda7032f3df734
tiredsosha <tiredsosha@gmail.com>
2025-05-19 22:23:33 +0300

    проектор дебаг

--- protocols/pjlink.go
+++ protocols/pjlink.go
@@ -30,12 +30,14 @@ func GetPjlink(ip string) int {
 	if err != nil {
 		logger.Error.Printf("couldn't send execute pjlink %v", err)
 	} else {
-		//logger.Info.Println("pjlink status -", status.Response)
-		boolStatus, _ := strconv.ParseBool(status.Response[0])
-		if boolStatus {
-			response = 200
-		} else {
-			response = 521
+		if len(status.Response) > 0 {
+			//logger.Info.Println("pjlink status -", status.Response)
+			boolStatus, _ := strconv.ParseBool(status.Response[0])
+			if boolStatus {
+				response = 200
+			} else {
+				response = 521
+			}
 		}
 	}
 	logger.Info.Println("pjlink status -", response)
